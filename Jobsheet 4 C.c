f17114492221ac37dbce114b72f005ff3fd3f793
InsanAdhibuanaPriyatna <64582598+InsanAdhibuanaPriyatna@users.noreply.github.com>
2020-05-13 21:24:09 -0700

    Create Jobsheet 4 C.c
    input nilai ke variabel dengan float dan double

---
+++ Jobsheet 4 C.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	float angka ;
+	double nilai ;
+	printf("Masukan Angka = ");
+	scanf ("%f",&angka);
+	printf("Masukan Nilai = ");
+	scanf("%d", nilai);
+	return 0;	
+}
